0b2ca7b177564e73887b7911931bdad4879e3f7e
Ayush Yeole <ayushyeole18@gmail.com>
2026-07-20 08:42:11 +0530

    Create E-88. Merge Sorted Array.java

---
+++ Arrays/1-D Arrays/E-88. Merge Sorted Array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int [] arr = new int[m+n];
+
+        for(int i=0; i<m; i++){
+            arr[i] = nums1[i];
+        }
+        for(int i=0; i<n; i++){
+            arr[m+i] = nums2[i];
+        }
+
+        Arrays.sort(arr);
+        int k = m+n;
+
+        for(int i=0; i<k; i++){
+            nums1[i] = arr[i];
+        }
+    }
+}

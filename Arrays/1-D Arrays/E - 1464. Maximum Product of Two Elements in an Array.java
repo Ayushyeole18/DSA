cc05a4134ae31ed03046addbdeee2beec72bbca7
Ayush Yeole <ayushyeole18@gmail.com>
2026-07-27 07:12:00 +0530

    Create E - 1464. Maximum Product of Two Elements in an Array.java

---
+++ Arrays/1-D Arrays/E - 1464. Maximum Product of Two Elements in an Array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int first = 0; 
+        int second = 0;
+
+        for(int ele : nums){
+            int digit = ele;
+
+            if(digit>first){
+                second = first;
+                first = digit;
+            }
+
+            else if(digit>second){
+                second = digit;
+            }
+        }
+        return (first-1) * (second-1);
+    }
+}
